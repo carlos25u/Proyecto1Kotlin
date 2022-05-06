9dd57202723ce1fdd1a347c0edf6b2f287c4076b
carlos lopez <88679267+carlos25u@users.noreply.github.com>
2022-05-06 16:19:03 -0400

    Haciendo PersonaDao

---
+++ app/src/main/java/com/call/proyecto1kotlin/data/PersonaDao.kt
@@ -0,0 +1,20 @@
+package com.call.proyecto1kotlin.data
+
+import androidx.room.*
+import com.call.proyecto1kotlin.model.Persona
+import java.util.concurrent.Flow
+
+@Dao
+interface PersonaDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun Insertar(persona:Persona)
+
+    @Delete
+    suspend fun Eliminar(persona: Persona)
+
+    @Query("SELECT * FROM Personas WHERE PersonaId = PersonaId")
+    fun Buscar(personaId:Int):Persona
+
+    @Query("SELECT * FROM Personas ORDER BY PersonaId")
+    fun GetLista():List<Persona>
+}
--- app/src/main/java/com/call/proyecto1kotlin/model/Persona.kt
+++ app/src/main/java/com/call/proyecto1kotlin/model/Persona.kt
@@ -4,7 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "Personas")
-data class Personas (
+data class Persona (
     @PrimaryKey(autoGenerate = true)
     val PersonaId:Int,
     val Nombres:String,
